c3edd231189c2761bb679109cc1d0eaac2de4273
= <=>
2021-02-21 04:27:28 -0500

    adding only view for login and logout and signup

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,8 @@ from .views import *
 app_name = 'account'
 
 urlpatterns = [
-
+                  path('signup/', SignUp.as_view(), name='sign_up'),
+                  path('login/', Login.as_view(), name='login'),
+                  path('logout/', Logout.as_view(), name='logout')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 # Create your views here.
+
+
+class SignUp(FormView):
+    template_name = 'sign_up.html'
+
+class Login(LoginView):
+    template_name = 'login.html'
+
+class Logout(LogoutView):
+    template_name = 'logout.html'
